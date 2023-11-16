b989454246afb9cfe7beccf758bbe0add45f7163
nurislamkalmanbetov <kalmanbetovnurislam19@gmail.com>
2023-11-16 17:09:41 +0600

    first commit

---
+++ apps/blog/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField("Название", max_length=255)
+    slug = models.SlugField(max_length=255)
+    icon = models.ImageField(upload_to="category/icons/")
+    
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        
+    def __str__(self):
+        return self.name 
+    
+
+class Post(models.Model):
+    title = models.CharField("Название", max_length=255)
+    description = models.TextField("Описание")
+    image = models.ImageField("Фото")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    is_draft = models.BooleanField("Черновик", default=True) 
+    
+    class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+
+    def __str__(self):
+        return self.title 
+    
+        
+        
+    
+    
---
+++ apps/blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from apps.blog import views 
+
+
+urlpatterns = [
+    path('', views.IndexPage.as_view(), name="index")
+]
