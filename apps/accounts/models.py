d5f7a4a05bcd6ce5b6cac50b43809d64fb26c3dd
nurislamkalmanbetov <kalmanbetovnurislam19@gmail.com>
2023-11-20 19:49:48 +0600

    adding AbstractUser, UserManager, BaseUserManager

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email=None, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        return user 
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField("Электронная почта", unique=True)
+    profile_img = models.ImageField(upload_to="/users/profiles/",blank=True)
+    about = models.TextField("О себе", null=True,blank=True)
+    instagram = models.URLField()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email 
+
